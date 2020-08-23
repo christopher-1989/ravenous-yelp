3e815474a69e3b92cdaba42279fb42541768fa43
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-08-23 11:08:38 +1000

    Part 3 of ravenous project complete. Added functionality for sorting buttons and feedback from searchbar to devs

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,65 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    "Best Match": "best_match",
-    "Highest Rated": "rating",
-    "Most Reviewed": "review_count"
-}
+
 
 class SearchBar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: "best_match"
+        };
+        this.sortByOptions = {
+            "Best Match": "best_match",
+            "Highest Rated": "rating",
+            "Most Reviewed": "review_count"
+        };
+        this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleTermChange = this.handleTermChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+    }
+    
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li 
+                        className={this.getSortByClass(sortByOptionValue)} 
+                        key={sortByOptionValue}
+                        onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                    >
+                            {sortByOption}
+                    </li>
         } )
+    }
+
+    getSortByClass(sortByOption){
+        if (this.state.sortBy === sortByOption) {
+            return 'active'
+        } else {
+            return ''
+        }
+    }
+
+    handleSortByChange(sortByOption){
+        this.setState({
+            sortBy: sortByOption
+        });
+    }
+    handleTermChange(event){
+        this.setState({
+            term: event.target.value
+        });
+    }
+    handleLocationChange(event){
+        this.setState({
+            location: event.target.value
+        });
+    }
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+        event.preventDefault()
     }
     render() {
         return (
@@ -23,11 +70,11 @@ class SearchBar extends React.Component{
                     </ul>
                 </div>
                 <div class="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
-                    <a href="www.#.com">Let's Go</a>
+                    <a onClick={this.handleSearch} href="www.#.com">Let's Go</a>
                 </div>
                 </div>
         )
